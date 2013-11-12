b8be6c8a58ff8294cd44d4258c8c1bd9ad145e80
Dustin <dalin@wl-10-190-51-56.wireless.duke.edu>
2013-11-12 00:21:47 -0500

    fixed bug in ConferenceParser

--- LOAD-TEST-QUERIES.sql
+++ LOAD-TEST-QUERIES.sql
@@ -22,3 +22,16 @@ SELECT *
 FROM GameStats
 LIMIT 10;
 
+-- list of conferences for main Conferences page
+SELECT name
+FROM Conference
+ORDER BY name;
+
+SELECT AVG(offensive_rebounds) AS ORPG, AVG(defensive_rebounds) AS DRPG, AVG(steals) AS SPG, 
+	AVG(assists) AS APG, AVG(personal_fouls) AS FPG,
+    SUM(cast(three_point_makes as float))/SUM(three_point_attempts) AS three_percent,
+    SUM(cast(two_point_makes as float))/SUM(two_point_attempts) AS two_percent,
+    SUM(cast(free_throw_makes as float))/SUM(free_throw_attempts) AS free_percent,
+    (SUM(cast(two_point_makes as float)) * 2 + SUM(three_point_makes) * 3)/COUNT(*) AS avg_points
+FROM GameStats
+WHERE player_id = 'c90c543f-1c4c-4d76-afd3-feda9ea83c3c' AND minutes <> '00:00';
