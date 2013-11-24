5cb5f90371addf20fb9ed708a19f00a54cf854d2
Dustin <dalin@wl-10-190-50-218.wireless.duke.edu>
2013-11-24 11:41:32 -0500

    updating data

---
+++ NCAAMB/NCAAMB/TeamViewController.h
@@ -0,0 +1,13 @@
+//
+//  TeamViewController.h
+//  NCAAMB
+//
+//  Created by Dustin Alin on 11/24/13.
+//  Copyright (c) 2013 Nicholas Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TeamViewController : UIViewController
+
+@end
---
+++ NCAAMB/NCAAMB/TeamViewController.m
@@ -0,0 +1,38 @@
+//
+//  TeamViewController.m
+//  NCAAMB
+//
+//  Created by Dustin Alin on 11/24/13.
+//  Copyright (c) 2013 Nicholas Gordon. All rights reserved.
+//
+
+#import "TeamViewController.h"
+
+@interface TeamViewController ()
+
+@end
+
+@implementation TeamViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
