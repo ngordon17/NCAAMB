543ba04fca805e25b2bf544d1e56cdd562a082c5
Nicholas Gordon <yankeenjg@wl-10-190-218-5.wireless.duke.edu>
2013-12-09 00:51:09 -0500

    created box score cell

---
+++ NCAAMB/NCAAMB/BoxScoreCell.h
@@ -0,0 +1,36 @@
+//
+//  BoxScoreCell.h
+//  NCAAMB
+//
+//  Created by Nicholas Gordon on 12/9/13.
+//  Copyright (c) 2013 Nicholas Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BoxScoreCell : UITableViewCell
+
+@property(nonatomic, strong) IBOutlet UILabel *name;
+@property(nonatomic, strong) IBOutlet UILabel *position;
+@property(nonatomic, strong) IBOutlet UILabel *min;
+@property(nonatomic, strong) IBOutlet UILabel *fgma;
+@property(nonatomic, strong) IBOutlet UILabel *three_pma;
+@property(nonatomic, strong) IBOutlet UILabel *ftma;
+@property(nonatomic, strong) IBOutlet UILabel *oreb;
+@property(nonatomic, strong) IBOutlet UILabel *dreb;
+@property(nonatomic, strong) IBOutlet UILabel *reb;
+@property(nonatomic, strong) IBOutlet UILabel *ast;
+@property(nonatomic, strong) IBOutlet UILabel *stl;
+@property(nonatomic, strong) IBOutlet UILabel *pf;
+@property(nonatomic, strong) IBOutlet UILabel *pts;
+
+
+
+
+
+
+
+
+
+
+@end
---
+++ NCAAMB/NCAAMB/BoxScoreCell.m
@@ -0,0 +1,29 @@
+//
+//  BoxScoreCell.m
+//  NCAAMB
+//
+//  Created by Nicholas Gordon on 12/9/13.
+//  Copyright (c) 2013 Nicholas Gordon. All rights reserved.
+//
+
+#import "BoxScoreCell.h"
+
+@implementation BoxScoreCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
